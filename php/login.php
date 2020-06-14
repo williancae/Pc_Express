d35db20bd7527da974a2ed7e901deb9a71a3856e
João Pedro Alves <jotalmeida007@hotmail.com>
2020-06-14 18:01:19 -0300

    Add a control session

--- php/login.php
+++ php/login.php
@@ -13,8 +13,10 @@ $resultado = $conexao->query($sql);
 if($resultado->num_rows>0){ 
     $_SESSION['email'] = $email; 
     $_SESSION['senha'] = $senha; 
-    header('location:index.html');
+    header('location:../index.html');
     }else{  
     session_unset();
     session_destroy();  
-    echo "<script>alert('Login ou Senha incorretos, tente novamente')  	window.location.href='login.html'</script>  	";  }
+    header('location=../index.html');
+}
+?>
