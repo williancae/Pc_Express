6a80b1f1c8977ef283e4f33e90137e76fd493c8a
João Pedro Alves <jotalmeida007@hotmail.com>
2020-06-15 09:48:32 -0300

    bug correction

--- php/contact-form.php
+++ php/contact-form.php
@@ -1,17 +1,21 @@
 <?php
 
+$GetParam = filter_input_array(INPUT_POST, FILTER_DEFAULT);
+
 //Variáveis
 $Nome = $GetParam['name'];
-$Email = $GetParam['Email'];
+$Email = $GetParam['email'];
 $Msg = $GetParam['message'];
 
+
 $Email_remetente = "jotalmeida005@outlook.com";
-$Email_destinatário = "jotalmeida007@hotmail.com"
+$Email_destinatário = "jotalmeida007@hotmail.com";
 $Email_reply = "{$Email}";
 
-$Email_assunto = "Novo contato pelo site"
+
+$Email_assunto = "Novo contato pelo site";
 $Email_conteudo = "Nome = {$Nome} \n";
-$Email_conteudo = "Email = {$Email} \n"
+$Email_conteudo = "Email = {$Email} \n";
 $Email_conteudo = "Mensagem = {$Msg} \n";
 $HeaderArray = array(
     "From: $Email_remetente",
@@ -20,14 +24,17 @@ $HeaderArray = array(
     "Return-path: $Email_remetente",
     "MIME-Version: 1.0",
     "X-Prioriyi: 3",
-    "Content-Type: text/html: charset=UTF-8"
-);
+    "Content-Type: text/html: charset=UTF-8");
 $Email_headers = implode("\n, $HeaderArray");
+
 
 if (mail($Email_destinatário, $Email_assunto, $Email_conteudo, $Email_headers)) {
     ?>
         <p>Sucesso, sua mensagem foi enviada</p>
+        <?php
 } else {
-    <p> Falha no envio da mensagem.
+    ?>
+    <p> Falha no envio da mensagem.</p>
+    <?php
 }
     
